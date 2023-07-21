d461f1bbcf236b4ed616c1df07894b0e4da6b517
mxnf12 <130280770+mxnf12@users.noreply.github.com>
2023-07-21 08:07:14 +0800

    Update common.ts

--- app/api/common.ts
+++ app/api/common.ts
@@ -9,11 +9,10 @@ const DISABLE_GPT4 = !!process.env.DISABLE_GPT4;
 export async function requestOpenai(req: NextRequest) {
   const controller = new AbortController();
   const authValue = req.headers.get("Authorization") ?? "";
-  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replace(
-  "/api/openai/",
-  `${baseUrl}/`,
+  const openaiPath = `${req.nextUrl.pathname}${req.nextUrl.search}`.replaceAll(
+  "https://api.mxnf.one",
+  "",
 );
-
 
   let baseUrl = BASE_URL;
 
